c1741b1a2b6262dc5ded93127c8df23113ebfebc
juliocesarr77 <julio.goiano@gmail.com>
2025-06-02 15:57:12 -0300

    FIX CRITICO: Corrigindo roteamento em App.js para usar CadastroPedido na rota /cliente/cadastro.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// src/App.js
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CorrigirStatus from './CorrigirStatus';
@@ -6,18 +8,18 @@ import Vendas from './components/Vendas'; // Página de "Vendas" para o cliente 
 import CadastroProduto from './components/CadastroProduto';
 import { PedidosProvider } from './context/PedidosContext';
 import Dashboard from './Dashboard';
-import CadastroPedido from './CadastroPedido';
+import CadastroPedido from './CadastroPedido'; // <-- ESTE É O COMPONENTE CERTO!
 import PrivateRoute from './PrivateRoute';
 import Home from './Home';
 import PaginaNaoEncontrada from './PaginaNaoEncontrada';
 import { AuthProvider } from './context/AuthContext';
 import Carrinho from './Carrinho';
-import CadastroCliente from './CadastroCliente';
+// import CadastroCliente from './CadastroCliente'; // <-- REMOVIDO!
 import ConfirmacaoPedido from './ConfirmacaoPedido';
 import AgradecimentoPedido from './AgradecimentoPedido';
-import PaginaDepoimentoSecreto from './components/PaginaDepoimentoSecreto'; // Importe o novo componente
-import AdminPage from './pages/AdminPage'; // Importe a página de administração
-import GerenciarDepoimentos from './admin/GerenciarDepoimentos'; // Importe o componente de gerenciamento de depoimentos
+import PaginaDepoimentoSecreto from './components/PaginaDepoimentoSecreto';
+import AdminPage from './pages/AdminPage';
+import GerenciarDepoimentos from './admin/GerenciarDepoimentos';
 
 function App() {
   return (
@@ -26,14 +28,15 @@ function App() {
         <Router>
           <Routes>
             {/* Rotas Públicas (Acessíveis a todos) */}
-            <Route path="/" element={<Home />} /> {/* Home para o cliente - SEM ClientLayout */}
-            <Route path="/vendas" element={<Vendas />} /> {/* Cliente vê os produtos/vendas aqui - SEM ClientLayout */}
+            <Route path="/" element={<Home />} />
+            <Route path="/vendas" element={<Vendas />} />
             <Route path="/carrinho" element={<Carrinho />} />
-            <Route path="/cliente/cadastro" element={<CadastroCliente />} />
+            {/* AQUI ESTÁ A CORREÇÃO CRÍTICA: */}
+            <Route path="/cliente/cadastro" element={<CadastroPedido />} /> 
             <Route path="/cliente/confirmacao" element={<ConfirmacaoPedido />} />
             <Route path="/cliente/agradecimento" element={<AgradecimentoPedido />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/depoimento/:token" element={<PaginaDepoimentoSecreto />} /> {/* Rota para o depoimento secreto */}
+            <Route path="/depoimento/:token" element={<PaginaDepoimentoSecreto />} />
             <Route path="*" element={<PaginaNaoEncontrada />} />
 
             {/* Rotas Privadas */}
@@ -43,8 +46,8 @@ function App() {
             <Route path="/admin/editar/:id" element={<PrivateRoute><CadastroPedido /></PrivateRoute>} />
             <Route path="/admin/cadastro-produto" element={<PrivateRoute><CadastroProduto /></PrivateRoute>} />
             <Route path="/admin/corrigir-status" element={<PrivateRoute><CorrigirStatus /></PrivateRoute>} />
-            <Route path="/admin" element={<PrivateRoute><AdminPage /></PrivateRoute>} /> {/* Rota para a página de administração */}
-            <Route path="/admin/depoimentos" element={<PrivateRoute><GerenciarDepoimentos /></PrivateRoute>} /> {/* Rota para gerenciar depoimentos */}
+            <Route path="/admin" element={<PrivateRoute><AdminPage /></PrivateRoute>} />
+            <Route path="/admin/depoimentos" element={<PrivateRoute><GerenciarDepoimentos /></PrivateRoute>} />
           </Routes>
         </Router>
       </PedidosProvider>
